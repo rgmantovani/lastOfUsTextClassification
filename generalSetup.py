fbdcda2b2047955267ecc2cf0fd76878ad6a01a3
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2024-11-25 20:01:18 -0300

    Update generalSetup.py
    Update on command executed by setup

--- generalSetup.py
+++ generalSetup.py
@@ -5,7 +5,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 NUM_CORES = 8
 
 def run_experiment(algoritmo, feature, dataset, seed):
-    cmd = ["python", "main.py", algoritmo, feature, dataset, str(seed)]
+    cmd = ["python3", "main.py", algoritmo, feature, dataset, str(seed)]
     result = subprocess.run(cmd, capture_output=True, text=True)
     return result.stdout, result.stderr
 
